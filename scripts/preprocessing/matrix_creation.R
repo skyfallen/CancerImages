9e805f32a95b23aef75b87607a336551ba5616a9
sdmytriv <solomiia.dmytriv@gmail.com>
2016-07-21 16:13:10 +0200

    create data matrix from greyscaled images

--- scripts/preprocessing/matrix_creation.R
+++ scripts/preprocessing/matrix_creation.R
@@ -1,23 +1,37 @@
 #START
-source("http://bioconductor.org/biocLite.R")
-biocLite("EBImage")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("EBImage")
 library(EBImage)
 
 setwd("data/images/resized512") #directory with images
-n <- 694 #number of images
+n <- 0
+for (name_i in list.files(pattern = "^[0-9, _]*\\.jpg$")){
+  n <- n+1
+}
 
 mat_img <- matrix(data = 0, nrow = n, ncol = (512*512+1), dimnames = NULL) #creation of the matrix 
 
-inc <- 1                                                                            #Creation! Fuck yeah, WE R GODs!
-for (name_i in list.files(pattern = "^[0-9, _]*\\.jpg$")){ #just use someee regular expressions ;)
-  x <-  readImage(name_i)
-  x_gray <- channel(x, "gray")
-  for(j in 1:(512*512)){
-    mat_img[inc, j] <- x[j]
-  }
-  inc <- inc+1
-}
+# init dataset
+dataset <- c()
 
-#now weve got a matrix of images mat_img
+
+dataset <- t(sapply(list.files(pattern = "^[0-9, _]*\\.jpg$"), function(file){ #just use someee regular expressions ;)
+  message(file)
+  x <-  readImage(file)
+  x_gray <- channel(x, "gray")
+  return(as.vector(t(as.array(x_gray))))
+}))
+
+
+dataset <- t(sapply(list.files(pattern = "^[0-9, _]*\\.jpg$"), function(file){ #just use someee regular expressions ;)
+  message(file)
+  x <-  readImage(file)
+  x_gray <- channel(x, "gray")
+  return(as.vector(t(as.array(x_gray))))
+}))
+
+
+setwd("../../..")
+
 
 
