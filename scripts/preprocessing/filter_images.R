537e1afa01e007a137b1cdf71c14fe848ac50be3
sdmytriv <solomiia.dmytriv@gmail.com>
2016-07-21 12:38:24 +0200

    Splitted percentage labels to 5 new classes

--- scripts/preprocessing/filter_images.R
+++ scripts/preprocessing/filter_images.R
@@ -12,7 +12,7 @@ library(BiocGenerics)
 ## read info_scorings.csv, detect null in positive_tumor_cells###
 mydata <- read.table("data/labels/info_scorings.csv", sep=";", header = TRUE,
                      na.strings = 'null')
-
+levels(mydata)
 # summary(mydata)
 picture <- mydata[is.na(mydata$positive_tumor_cells),]$pic
 length(picture)
