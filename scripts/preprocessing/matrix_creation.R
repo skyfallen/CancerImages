b99abcda286df6a0ebbb979ea944bba28d1b7baa
Olga Tatarintseva <olitatarintseva@gmail.com>
2016-07-22 15:40:23 +0300

    Ability to save RDS added

--- scripts/preprocessing/matrix_creation.R
+++ scripts/preprocessing/matrix_creation.R
@@ -1,5 +1,5 @@
 library(EBImage)
-
+library(caret)
 dataset <- c()
 dataset <- t(sapply(list.files(path = "data/images/resize256", 
                                pattern = "^[0-9, _]*\\.jpg$", full.names = TRUE), function(file){ #just use someee regular expressions ;)
@@ -12,11 +12,9 @@ rownames(dataset) = sub('.*/', '', rownames(dataset))
 dataset = as.data.frame(dataset)
 matching<-read.table("data/labels/info_scorings_filtered.csv", header = TRUE)
 dataset$Label = matching$newLabel[which(matching$pic %in% rownames(dataset))]
-
-
-library(caret)
-# nearZeroVar(dataset)
+nearZeroVar(dataset)
 dataset <- dataset[,-nearZeroVar(dataset)]
-# dim(dataset)
-
-write.table(dataset, file="data/labels/workingMatrix.csv")
+write.table(dataset, file="data/labels/workingMatrix.csv" )
+#saveRDS(object = dataset, file = 'data/labels/workingMatrix.RDS')
+# The way to load RDS file
+# dataset = loadRDS('data/labels/workingMatrix.RDS')
